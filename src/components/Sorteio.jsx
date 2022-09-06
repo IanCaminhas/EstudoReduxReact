ccf111897f173b0f459de0f4f76144c7aafe1bcf
IanPC <iancaminhas25@hotmail.com>
2022-09-06 20:14:38 -0300

    Outros componentes

---
+++ src/components/Sorteio.jsx
@@ -0,0 +1,18 @@
+import './Intervalo.css'
+import React from 'react'
+import Card from './Card'
+
+
+export default props =>{
+    return (
+        <Card title="Sorteio de um Número" purple>
+            <div>
+             <span>
+                 <span>Resultado:</span>
+                 <strong>8</strong>
+             </span>
+            </div>
+            
+        </Card>
+    )
+}
